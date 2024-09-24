29a989ec66ab7e1e8decaf1327445c92e650c0d9
nicomp42 <nicholdw@ucmail.uc.edu>
2024-09-24 07:33:00 -0400

    Add project files.

---
+++ StatesAndCapitals/main.py
@@ -0,0 +1,10 @@
+
+import csv
+
+with open('StatesAndCapitals.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    header = next(csvreader) # skip the header row.
+    for row in csvreader:
+        state = row[0]
+        capital = row[1]
+        print(f"State: {state}, Capital: {capital}")
